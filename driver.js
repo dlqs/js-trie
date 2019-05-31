5bc94169237bad54160a978e0f9637dedc8977bf
dlqs <donaldleeqs@gmail.com>
2019-06-01 04:04:02 +0800

    more stuff

--- driver.js
+++ driver.js
@@ -8,12 +8,16 @@ document.onreadystatechange = function () {
 var main = function() {
     const searchBox = document.getElementById('searchBox')
     var trie = new Trie()
-    var text = document.getElementById('content').textContent.split(' ')
-    console.time('trieloadstart')
+    var text = document.getElementById('content').textContent
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") // remove punctuation
+        .replace(/\n/g, " ") // remove newlines
+        .split(" ")
+    const t0 = performance.now()
     text.forEach(function(word) {
-        trie.insert(word)
+        trie.insert(word.toLowerCase())
     })
-    console.timeEnd('trieloadstart')
+    const t1= performance.now()
+    refreshTimeIndex(t1 - t0)
 
     searchBox.addEventListener("keyup", function(event) {
         if (event.keyCode !== 13) { // 13 is Enter
@@ -21,10 +25,29 @@ var main = function() {
         }
         event.preventDefault();
         var searchText = searchBox.value
-        console.time('triecontainsstart')
-        const res = trie.contains(searchText)
-        console.timeEnd('triecontainsstart')
-        console.log(res)
+        const t2 = performance.now()
+        const res = trie.getSubTrie(searchText)
+        const t3 = performance.now()
+        refreshTimeSearch(t3 - t2)
+        refreshList(res)
     })
     console.log('Main completed.')
+}
+
+var refreshList = function (arr) {
+    document.getElementById("myList").innerHTML = ''
+    arr.forEach(function (element) {
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(element.toString());         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("myList").appendChild(node);
+    })
+}
+
+var refreshTimeIndex = function(i) {
+    document.getElementById('timeIndex').textContent = i.toString()
+}
+
+var refreshTimeSearch = function(i) {
+    document.getElementById('timeSearch').textContent = i.toString()
 }
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Trie {
     constructor() {
         this.root = null
     }
-    // Inserts a string into the trie
+    // Inserts str into the trie
     insert(str) {
         if (str.length === 0) {
             return
@@ -44,6 +44,7 @@ class Trie {
         this._insert(str.slice(1), node.getChildChar(charToAdd))
     }
 
+    // Return a boolean if the str exists as terminal word
     contains(str) {
         if (str.length === 0) return false
         return this._contains(str, this.root)
@@ -59,5 +60,38 @@ class Trie {
         }
         return this._contains(str.slice(1), node.getChildChar(charToCheck))
     }
+
+    // Return sub-tree whose root(s) is str
+    getSubTrie(str) {
+        if (str.length === 0) return []
+        return this._getSubtrie(str, str, this.root, [])
+    }
+
+    _getSubtrie(prefix, str, node, arr) {
+        if (str.length === 0) {
+            // matched, DFS to return all terminals in this sub trie
+            if (node.freq > 0) {
+                arr.push(prefix)
+            }
+            const children = node.children
+            for (let key of Object.keys(children)) {
+                this._getSubtrie(prefix + key, str.slice(1), children[key], arr)
+            }
+        }
+        const charToCheck = str.charAt(0)
+        if (!node.hasChildChar(charToCheck)) {
+            return arr
+        }
+        return this._getSubtrie(prefix, str.slice(1), node.getChildChar(charToCheck), arr)
+    }
 }
 
+//const trie = new Trie()
+//trie.insert('bash')
+//trie.insert('babe')
+//trie.insert('baby')
+//trie.insert('baa')
+//trie.insert('bb')
+//
+//console.log(trie.getSubTrie('bab'))
+//console.log(trie.getSubTrie('b'))
